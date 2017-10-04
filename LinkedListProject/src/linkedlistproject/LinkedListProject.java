e22606d0244376b30f9fa3c9d457cfa28905f115
pwd <pwd@bppwd.com>
2017-10-04 10:04:58 -0400

    Base-line Class Structure

---
+++ LinkedListProject/src/linkedlistproject/LinkedListProject.java
@@ -0,0 +1,15 @@
+/*
+    Authors: Jamie & Nate
+    Date: 10 / 4 / 2017
+ */
+package linkedlistproject;
+
+public class LinkedListProject {
+    Node head;
+    public LinkedListProject(Node a) {
+        head = a;
+    }
+    public void addFirst(String value) {
+        
+    }
+}
---
+++ LinkedListProject/src/linkedlistproject/Node.java
@@ -0,0 +1,9 @@
+/*
+    Authors: Jamie & Nate
+    Date: 10 / 4 / 2017
+ */
+package linkedlistproject;
+
+public class Node {
+    
+}
